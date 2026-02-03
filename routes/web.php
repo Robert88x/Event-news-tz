cfb3195e4631e39d3dc6bb1d208dc401604aead2
Robert <robert@Roberts-MacBook-Air.local>
2026-02-03 14:43:26 +0300

    update paths

--- routes/web.php
+++ routes/web.php
@@ -32,3 +32,9 @@ Route::middleware('auth')->group(function () {
 require __DIR__.'/auth.php';
 
 // Route::get('/chat', Index::class)->name('chat.index');
+
+// Route::middleware('auth')->group(function () {
+//     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+//     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+//     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+// });
